4a1cebeaf7b2f7387a547c0273bf8627497ad488
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-03-16 08:07:42 -0300

    feat: :sparkles: first commit

---
+++ Minimal-Api/Dominio/DTO's/LoginDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Minimal_Api.Dominio.DTO_s {
+	public class LoginDTO {
+		public string Email { get; set; } = default!;
+		public string Senha { get; set; } = default!;
+	}
+}
---
+++ Minimal-Api/Program.cs
@@ -0,0 +1,40 @@
+using Minimal_Api.Dominio.DTO_s;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/", () => "Olá pessoal")
+.WithName("Hello")
+.WithOpenApi();
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+	if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456") {
+		return Results.Ok("Login com sucesso");
+	} else {
+		return Results.Unauthorized();
+	}
+});
+
+app.Run();
+
+
+
+
+
+
